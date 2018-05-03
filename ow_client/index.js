26accdf21772dda75767b46aab5a30275eba87cc
Lewis Daly <lewisdaly@me.com>
2018-05-03 10:12:42 +0930

    restructure project, navigator setup

--- ow_client/index.js
+++ ow_client/index.js
@@ -6,20 +6,26 @@
 
 
 import { Navigation } from 'react-native-navigation';
-import { registerScreens } from './screens';
+import { registerScreens } from './src/screens';
 
 registerScreens(); // this is where you register all of your app's screens
 
 // start the app
-Navigation.startTabBasedApp({
-  tabs: [
-    {
-      label: 'One',
-      screen: 'example.FirstTabScreen', // this is a registered name for a screen
-      icon: require('./assets/blue_marker.png'),
-      // selectedIcon: require('../img/one_selected.png'), // iOS only
-      title: 'Screen One'
-    },
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'example.FirstTabScreen', // unique ID registered with Navigation.registerScreen
+    // title: 'Welcome', // title of the screen as appears in the nav bar (optional)
+    navigatorStyle: {}, // override the navigator style for the screen, see "Styling the navigator" below (optional)
+    navigatorButtons: {} // override the nav buttons for the screen, see "Adding buttons to the navigator" below (optional)
+  },
+  // tabs: [
+  //   {
+  //     label: 'One',
+  //     screen: 'example.FirstTabScreen', // this is a registered name for a screen
+  //     icon: require('./assets/blue_marker.png'),
+  //     // selectedIcon: require('../img/one_selected.png'), // iOS only
+  //     title: 'Screen One'
+  //   },
     // {
     //   label: 'Two',
     //   screen: 'example.SecondTabScreen',
@@ -27,5 +33,5 @@ Navigation.startTabBasedApp({
     //   selectedIcon: require('../img/two_selected.png'), // iOS only
     //   title: 'Screen Two'
     // }
-  ]
+  // ]
 });
---
+++ ow_client/src/screens/index.js
@@ -0,0 +1,12 @@
+import { Navigation } from 'react-native-navigation';
+
+import App from '../App';
+// import SecondTabScreen from './SecondTabScreen';
+// import PushedScreen from './PushedScreen';
+
+// register all screens of the app (including internal ones)
+export function registerScreens() {
+  Navigation.registerComponent('example.FirstTabScreen', () => App);
+  // Navigation.registerComponent('example.SecondTabScreen', () => SecondTabScreen);
+  // Navigation.registerComponent('example.PushedScreen', () => PushedScreen);
+}
