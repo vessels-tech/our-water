8c9e06d8fd2757612ef2ad3f8fd4e9049723adce
Lewis Daly <lewisdaly@me.com>
2018-07-11 10:32:24 +0930

    fixed initial load

--- ow_client/_run_prod.sh
+++ ow_client/_run_prod.sh
@@ -6,4 +6,5 @@ export ENVFILE="$DIR"/../env/.env.sh
 
 react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
 
-react-native run-android --no-packager
+# react-native run-android --no-packager
+react-native run-android --no-packager && clear & make start
