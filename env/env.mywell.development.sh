7d46d283daacf359e1e8daacc3b8722558292582
Lewis Daly <lewisdaly@me.com>
2018-10-03 15:18:20 +0930

    working on build env

---
+++ env/env.mywell.development.sh
@@ -0,0 +1,17 @@
+
+export REACT_APP_ORG_ID="mywell"
+export REACT_APP_FB_AUTH_DOMAIN="our-water.firebaseapp.com"
+export REACT_APP_FB_DATABASE_URL="https://our-water.firebaseio.com"
+export REACT_APP_FB_PROJECT_ID="our-water"
+export REACT_APP_FB_STORAGE_BUCKET="our-water.appspot.com"
+export REACT_APP_BASE_URL="https://us-central1-our-water.cloudfunctions.net"
+
+#config timeout is in seconds
+export REACT_APP_REMOTE_CONFIG_TIMEOUT=600
+export ENABLE_LOGGING='true'
+
+#Remote config setup
+export SHOULD_USE_LOCAL_CONFIG='true'
+
+#Just for now, as we are still sorting this out.
+export CONFIG_TYPE='GGMNDevConfig'
