07ef5ce49e29d16ae457149e6ebdf955d41a808b
Lewis Daly <lewisdaly@me.com>
2018-10-13 17:05:19 +1030

    got hockeyapp build working from make

--- env/increment_build.sh
+++ env/increment_build.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 lastBuildNumber=`cat "$DIR"/buildnumber`
-echo "$((lastBuildNumber + 1))" > "$DIR"/buildnumber
+echo -n "$((lastBuildNumber + 1))" > "$DIR"/buildnumber
