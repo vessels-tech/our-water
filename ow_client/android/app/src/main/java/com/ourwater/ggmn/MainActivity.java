cb9b6e1a7cdd929f558430bf27b57816ae306a9f
Lewis Daly <lewisdaly@me.com>
2018-10-15 22:23:23 +1030

    fixing blank build

--- ow_client/android/app/src/main/java/com/ourwater/ggmn/MainActivity.java
+++ ow_client/android/app/src/main/java/com/ourwater/ggmn/MainActivity.java
@@ -1,15 +1,17 @@
 package com.vesselstech.ggmn;
 
+import android.graphics.Color;
+import android.view.View;
+
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    // @Override
-    // protected String getMainComponentName() {
-    //     return "OurWater";
-    // }
+    @Override
+    public View createSplashLayout() {
+        View view = new View(this);
+        view.setBackgroundColor(Color.BLUE);
+        return view;
+}
+
 }
