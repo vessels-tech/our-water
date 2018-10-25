311b11d273a3f903a40d2bfcbb2aff64cda841df
Lewis Daly <lewisdaly@me.com>
2018-10-25 11:54:50 +1030

    giving up on jsx dream

--- ow_firebase/functions/lib/fn_sos/XmlBuilder.js
+++ ow_firebase/functions/lib/fn_sos/XmlBuilder.js
@@ -12,7 +12,8 @@ var _jsxToHtml = require("jsx-to-html");
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
 function testTsx() {
-  return (0, _jsxToHtml.render)(React.createElement("div", {
+  return (0, _jsxToHtml.render)(React.createElement("xml", {
+    tag: "123",
     className: "Hello World"
   }, "Hello World"));
 }
