852f988c79d238d8b7a86d9633dd2f9fe7761583
Lewis Daly <lewisdaly@me.com>
2019-01-21 21:47:44 +1030

    GGMN Build is broken

--- ow_firebase/functions/lib/tools/index.js
+++ ow_firebase/functions/lib/tools/index.js
@@ -304,6 +304,11 @@ function getNewConfig() {
                 ggmnTranslationsJSON,
                 mywellTranslationsJSON,
             ]),
+            ggmn_ignoreReading: buildParameter(JSON.stringify({ date: "2017-01-01T01:11:01Z", value: 0 }, null, 2), 'A reading in GGMN that should be ignored by the graphs', conditionKeys, [
+                JSON.stringify({ date: "2017-01-01T01:11:01Z", value: 0 }, null, 2),
+                JSON.stringify({ date: "2017-01-01T01:11:01Z", value: 0 }, null, 2),
+                JSON.stringify({ date: "2017-01-01T01:11:01Z", value: 0 }, null, 2),
+            ])
         };
         return Promise.resolve({
             conditions,
