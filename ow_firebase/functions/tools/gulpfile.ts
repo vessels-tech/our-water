9f9053fece6a4c986abd0629e98a78adf86a4264
Lewis Daly <lewisdaly@me.com>
2019-02-04 21:50:09 +0800

    Add a hacky method of upload csvs

--- ow_firebase/functions/tools/gulpfile.ts
+++ ow_firebase/functions/tools/gulpfile.ts
@@ -11,8 +11,8 @@ import { readingToCSV, readingHeading } from './csv';
 var fs = require('fs');
 const fbApi = new FirebaseApi(firestore);
 const key = require('../src/test/.serviceAccountKey.json');
-const baseUrl = process.env.BASE_URL;
-const orgId = process.env.ORG_ID;
+const baseUrl = process.env.REACT_APP_BASE_URL;
+const orgId = process.env.REACT_APP_ORG_ID;
 
 const PROJECT_ID = 'our-water';
 
@@ -121,7 +121,23 @@ gulp.task('get_readings_csv', async() => {
 gulp.task('upload_readings_from_csv', async () => {
   const files = [
     //Place your links to firebase files here.
-    'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2017.xlsx%20-%20B-Well.tsv?alt=media&token=1e17d48f-5404-4f27-90f3-fb6a76a6dc45',
+    // eg: 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/test_1.csv?alt=media&token=66630fd9-09d6-40b3-9d0b-07151f8e8f93'
+      // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/test_2.csv?alt=media&token=4d347909-8daf-41c6-8a2e-5340d3bac896',
+      
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20V-Well.csv?alt=media&token=f07c5115-5341-4c56-bf62-7bfc798b9eaa',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20V%20Rainfall.csv?alt=media&token=c9138579-ca8a-4920-8aff-84cd67cf3a4a',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20S-Checkdam.csv?alt=media&token=42376502-bf5a-47d7-8466-f3e730e0897a',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20S-Well.csv?alt=media&token=73ac0dad-4694-4f8a-8a5b-8d5243311a9c',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20S-Rainfall.csv?alt=media&token=13316d4f-90f6-4afe-a2ea-7da4c55294fa',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20H-Well.csv?alt=media&token=ad8b5c85-1e8e-4af5-9177-7ab5b5c2d913',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20H-Rainfall.csv?alt=media&token=e6b63576-3df0-4b3a-827e-624480f92495',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20H-checkdam.csv?alt=media&token=00b45ffd-113b-4631-8ac5-0742bd767e22',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20D-Well.csv?alt=media&token=f7738a43-b735-4616-8de9-1468225e53fe',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20D-Rainfall.csv?alt=media&token=1580ad26-3edd-448c-8985-bb0355f6280c',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20D-Checkdam.csv?alt=media&token=d7f24e83-319d-4a41-a947-0f0c5dbb02fe',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20B-Well.csv?alt=media&token=1342615f-67dd-41d0-8cca-4b3ff8a75a77',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20B-Rainfall.csv?alt=media&token=75ece5f8-448e-4bb8-bc7f-8bcbe7198195',
+    // 'https://firebasestorage.googleapis.com/v0/b/our-water.appspot.com/o/MywelluploadDharta2018-19.xlsx%20-%20B-Checkdam.csv?alt=media&token=323434e3-970a-4404-a9b3-8f6c84cccc93',
   ];
 
   //Build the sync requests
@@ -157,10 +173,24 @@ gulp.task('upload_readings_from_csv', async () => {
     return createSyncOptions;
   });
 
-  const responses = await Promise.all(syncOptions.map(options => request(options)
-    .then((response: any) => JSON.parse(response))
+  const syncIds = await Promise.all(syncOptions.map(options => request(options)
+    .then((response: any) => response.data.syncId)
     .catch(err => console.log(err))
   ));
 
-  console.log("Create syncs responses are: ", responses);
+  const syncRunOptions: any[] = syncIds.map(syncId => ({
+    method: 'POST',
+    uri: `${baseUrl}/sync/${orgId}/run/${syncId}?method=pullFrom`
+  }));
+
+  console.log("Create syncs responses are: ", syncIds);
+
+  //Now run the syncs
+  const syncRuns = await Promise.all(syncRunOptions.map(options => request(options)
+    .then((response: any) => response)
+    .catch(err => console.log(err))
+  ));
+
+  console.log("syncRuns", syncRuns);
+
 });
