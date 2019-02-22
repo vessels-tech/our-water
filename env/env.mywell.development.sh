44770fc9a3807101e55e5c641d9cb7a3b3511e7a
Lewis Daly <lewisdaly@me.com>
2019-02-22 12:13:26 +0800

    Add crashlytics config to debug RN Crashes

--- env/env.mywell.development.sh
+++ env/env.mywell.development.sh
@@ -17,7 +17,7 @@ export ENABLE_CACHE='true'
 export RESOURCE_CACHE_MAX_SIZE=350
 
 #Remote config setup
-export SHOULD_USE_LOCAL_CONFIG='false'
+export SHOULD_USE_LOCAL_CONFIG='true'
 
 #Just for now, as we are still sorting this out.
 export CONFIG_TYPE='MyWellDevConfig'
